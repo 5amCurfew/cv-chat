8dd112dc1cf3745652e41fd749c6dc0ef0b480bf
Samuel Knight <samueltobyknight@gmail.com>
2021-03-16 00:43:48 +0000

    introduce tfjs toxicity

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,28 @@
+var socket = io();
+        
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var displayName = document.getElementById('name');
+var input = document.getElementById('input');
 
+// CLIENT EMIT chat message ON SUBMIT to SERVER
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+        const msg = {
+            sender: displayName.value === undefined || displayName.value === '' ? socket.id : displayName.value,
+            text: input.value
+        };
+        socket.emit('chat message', msg);
+        input.value = '';
+    }
+});
+
+// CLIENT RECEIVE chat message FROM SERVER
+socket.on('chat message', function(msg) {
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
 
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+require('@tensorflow/tfjs-node');
+const toxicity = require('@tensorflow-models/toxicity');
 
 app.use(express.static('public'));
 
@@ -12,20 +14,37 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     // Show connect
-    console.log(`a user connected on :: ${socket.id}`);
-    io.emit('chat message', `--- New chatter joined ${socket.id} ---`);
+    console.log(`connected :: ${socket.id}`);
+    io.emit('chat message', `--- ${socket.id} joined ${String.fromCodePoint(0x1F37B)} ---`);
 
     // Show disconnect
     socket.on('disconnect', () => {
-      console.log(`${socket.id} :: user disconnected`);
+      console.log(`${socket.id} :: disconnected`);
+      io.emit('chat message', `--- ${socket.id} left ${String.fromCodePoint(0x1F695)} ---`);
     });
 
     // RECEIVE chat message from CLIENT THEN ...
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+      console.log(msg);
+      
+      toxicity.load(0.8).then(model => {
+      
+        model.classify(msg.text).then(predictions => {
+          let matches = predictions.filter( (p) => p.results[0].match === true);
+          console.log(matches);
+
+          if(matches.length > 0){
+            io.emit('chat message', `${msg.sender}: ${String.fromCodePoint(0x1F6AB).repeat(3)}`);
+          } else{
+            io.emit('chat message', `${msg.sender}: ${msg.text}`);
+          }
+
+        });
+      });
+
     });
 });
 
 http.listen(PORT, () => {
-  console.log(`Socket.IO server running at http://localhost:${PORT}/`);
+  console.log(`cv-chat socket.io server running at http://localhost:${PORT}/`);
 });
